53f8eed5423475e193f63df39491a35c2cdbd0e5
hyorenju <hieu.oranger@gmail.com>
2023-10-09 11:48:13 +0700

    add system.properties for deploying

--- student/src/main/java/vn/edu/vnua/fita/student/service/admin/file/FirebaseService.java
+++ student/src/main/java/vn/edu/vnua/fita/student/service/admin/file/FirebaseService.java
@@ -1,32 +1,23 @@
 package vn.edu.vnua.fita.student.service.admin.file;
 
 import com.google.cloud.storage.*;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import vn.edu.vnua.fita.student.common.FirebaseExpirationTimeConstant;
 import vn.edu.vnua.fita.student.config.FirebaseConfig;
-import vn.edu.vnua.fita.student.config.TwilioConfig;
-import vn.edu.vnua.fita.student.request.student.UpdatePhoneNumberRequest;
 import vn.edu.vnua.fita.student.service.admin.iservice.IFirebaseService;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.DecimalFormat;
-import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
 public class FirebaseService implements IFirebaseService {
     private final FirebaseConfig firebaseConfig;
-    private final TwilioConfig twilioConfig;
 
     @Override
     public Blob uploadImage(MultipartFile file, String bucketName) throws IOException {
@@ -72,31 +63,10 @@ public class FirebaseService implements IFirebaseService {
                 .toString();
     }
 
-    @Override
-    public void sendOTP(UpdatePhoneNumberRequest request) {
-        try {
-            PhoneNumber to = new PhoneNumber(request.getPhoneNumber());
-            PhoneNumber from = new PhoneNumber(twilioConfig.getTrialNumber());
-            String otp = generateOTP();
-            String otpMessage = "Bạn đã yêu cầu cập nhật số điện thoại. Mã OTP của bạn là " + otp + ", vui lòng không chia sẻ mã này cho bất kỳ ai. Nếu bạn không yêu cầu điều này, vui lòng bỏ qua tin nhắn.";
-
-            System.out.println("Sending OTP " + otp + " to " + request.getPhoneNumber());
-
-            Message.creator(to, from, otpMessage).create();
-        } catch (Exception e) {
-            throw new RuntimeException("K thể gửi sms");
-        }
-    }
-
     private String getFileExtension(String filename) {
         if (filename != null && filename.lastIndexOf(".") != -1) {
             return filename.substring(filename.lastIndexOf("."));
         }
         return "";
-    }
-
-    private String generateOTP() {
-        return new DecimalFormat("000000")
-                .format(new Random().nextInt(999999));
     }
 }
--- student/src/main/java/vn/edu/vnua/fita/student/service/admin/iservice/IFirebaseService.java
+++ student/src/main/java/vn/edu/vnua/fita/student/service/admin/iservice/IFirebaseService.java
@@ -2,13 +2,10 @@ package vn.edu.vnua.fita.student.service.admin.iservice;
 
 import com.google.cloud.storage.Blob;
 import org.springframework.web.multipart.MultipartFile;
-import com.twilio.rest.api.v2010.account.Message;
-import vn.edu.vnua.fita.student.request.student.UpdatePhoneNumberRequest;
 
 import java.io.IOException;
 
 public interface IFirebaseService {
     Blob uploadImage(MultipartFile file, String bucketName) throws IOException;
     String uploadFileExcel(String filePath, String bucketName) throws IOException;
-    void sendOTP(UpdatePhoneNumberRequest request);
 }
