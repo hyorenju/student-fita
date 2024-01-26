54e0fc4b7d7a5029fbbabe4ecc0edd9017aa93fe
hyorenju <hieu.oranger@gmail.com>
2024-01-26 10:25:21 +0700

    push for deploy part 2

--- student/src/main/java/vn/edu/vnua/fita/student/config/FirebaseConfig.java
+++ student/src/main/java/vn/edu/vnua/fita/student/config/FirebaseConfig.java
@@ -1,27 +1,28 @@
 package vn.edu.vnua.fita.student.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Value("${firebase.config.path}") // Đường dẫn tới file config JSON
-    private String firebaseConfigPath;
+    @Value("${firebase.config.name}") // Đường dẫn tới file config JSON
+    private String firebaseConfigName;
 
     @Bean
     public GoogleCredentials getCredentials() throws IOException {
         // Xác thực google firebase
-        InputStream serviceAccountKey = new FileInputStream(firebaseConfigPath);
-        return GoogleCredentials.fromStream(serviceAccountKey);
+        try (InputStream serviceAccountKey = new ClassPathResource(firebaseConfigName).getInputStream()){
+            return GoogleCredentials.fromStream(serviceAccountKey);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
 //    @Bean
